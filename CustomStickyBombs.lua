2e876c2a87087828bd72457f8efcf63186d0a00b
George0828Zhang <cc.chang0828@gmail.com>
2023-08-29 15:11:58 +0800

    increased lod dist. disable custom exp by default (network issue).

--- CustomStickyBombs.lua
+++ CustomStickyBombs.lua
@@ -16,10 +16,12 @@ local offset = c4_md_presets[c4_md_choice][2]
 local rot = c4_md_presets[c4_md_choice][3]
 
 local c4_exp_presets = {
+	[-1] = "Disabled",
+	[0] = "GRENADE",
 	-- See https://altv.stuyk.com/docs/articles/tables/explosions.html
-	"GRENADE","GRENADELAUNCHER","STICKYBOMB","MOLOTOV","ROCKET","TANKSHELL","HI_OCTANE","CAR","PLANE","PETROL_PUMP","BIKE","STEAM","FLAME","WATER_HYDRANT","GAS_CANISTER","BOAT","SHIP_DESTROY","TRUCK","BULLET","SMOKEGRENADELAUNCHER","SMOKEGRENADE","BZGAS","FLARE","GAS_CANISTER","EXTINGUISHER","PROGRAMMABLEAR","TRAIN","BARREL","PROPANE","BLIMP","FLAME_EXPLODE","TANKER","PLANE_ROCKET","VEHICLE_BULLET","GAS_TANK","BIRD_CRAP","RAILGUN","BLIMP2","FIREWORK","SNOWBALL","PROXMINE","VALKYRIE_CANNON","AIR_DEFENSE","PIPEBOMB","VEHICLEMINE","EXPLOSIVEAMMO","APCSHELL","BOMB_CLUSTER","BOMB_GAS","BOMB_INCENDIARY","BOMB_STANDARD","TORPEDO","TORPEDO_UNDERWATER","BOMBUSHKA_CANNON","BOMB_CLUSTER_SECONDARY","HUNTER_BARRAGE","HUNTER_CANNON","ROGUE_CANNON","MINE_UNDERWATER","ORBITAL_CANNON","BOMB_STANDARD_WIDE","EXPLOSIVEAMMO_SHOTGUN","OPPRESSOR2_CANNON","MORTAR_KINETIC","VEHICLEMINE_KINETIC","VEHICLEMINE_EMP","VEHICLEMINE_SPIKE","VEHICLEMINE_SLICK","VEHICLEMINE_TAR","SCRIPT_DRONE","RAYGUN","BURIEDMINE","SCRIPT_MISSILE"
+	"GRENADELAUNCHER","STICKYBOMB","MOLOTOV","ROCKET","TANKSHELL","HI_OCTANE","CAR","PLANE","PETROL_PUMP","BIKE","STEAM","FLAME","WATER_HYDRANT","GAS_CANISTER","BOAT","SHIP_DESTROY","TRUCK","BULLET","SMOKEGRENADELAUNCHER","SMOKEGRENADE","BZGAS","FLARE","GAS_CANISTER","EXTINGUISHER","PROGRAMMABLEAR","TRAIN","BARREL","PROPANE","BLIMP","FLAME_EXPLODE","TANKER","PLANE_ROCKET","VEHICLE_BULLET","GAS_TANK","BIRD_CRAP","RAILGUN","BLIMP2","FIREWORK","SNOWBALL","PROXMINE","VALKYRIE_CANNON","AIR_DEFENSE","PIPEBOMB","VEHICLEMINE","EXPLOSIVEAMMO","APCSHELL","BOMB_CLUSTER","BOMB_GAS","BOMB_INCENDIARY","BOMB_STANDARD","TORPEDO","TORPEDO_UNDERWATER","BOMBUSHKA_CANNON","BOMB_CLUSTER_SECONDARY","HUNTER_BARRAGE","HUNTER_CANNON","ROGUE_CANNON","MINE_UNDERWATER","ORBITAL_CANNON","BOMB_STANDARD_WIDE","EXPLOSIVEAMMO_SHOTGUN","OPPRESSOR2_CANNON","MORTAR_KINETIC","VEHICLEMINE_KINETIC","VEHICLEMINE_EMP","VEHICLEMINE_SPIKE","VEHICLEMINE_SLICK","VEHICLEMINE_TAR","SCRIPT_DRONE","RAYGUN","BURIEDMINE","SCRIPT_MISSILE"
 }
-local c4_exp_choice = 60
+local c4_exp_choice = -1 --60
 local explode_thres = 975
 
 log.info("Hash "..tostring(c4_md_presets[c4_md_choice][1]).." valid: "..tostring(STREAMING.IS_MODEL_VALID(after)))
@@ -44,12 +46,12 @@ Tab:add_imgui(function()
 		ImGui.EndCombo()
 	end
 	-- ImGui.SameLine()
-	if ImGui.BeginCombo("C4 Explosion", tostring(c4_exp_presets[c4_exp_choice+1]), ImGuiComboFlags.PopupAlignLeft) then
+	if ImGui.BeginCombo("C4 Explosion", tostring(c4_exp_presets[c4_exp_choice]), ImGuiComboFlags.PopupAlignLeft) then
 		for k, v in pairs(c4_exp_presets) do
-			if ImGui.Selectable(v, k == c4_exp_choice + 1) then
-				c4_exp_choice = k - 1
+			if ImGui.Selectable(v, k == c4_exp_choice) then
+				c4_exp_choice = k
 			end
-			if k == c4_exp_choice + 1 then
+			if k == c4_exp_choice then
 				ImGui.SetItemDefaultFocus()
 			end
 		end
@@ -79,12 +81,12 @@ function spawn_obj(sc, entity, hash, playerPed)
 	-- This prevents collision with player when throwing
 	-- while also making thrown ones destroyable
 	-- *can also use this one: IS_PROJECTILE_TYPE_WITHIN_DISTANCE() to differenciate
-	if ENTITY.IS_ENTITY_ATTACHED_TO_ENTITY(entity, playerPed) then
+	if ENTITY.IS_ENTITY_ATTACHED_TO_ENTITY(entity, playerPed) or c4_exp_choice < 0 then
 		ENTITY.SET_ENTITY_COLLISION(pj, false, false)
-		ENTITY.FREEZE_ENTITY_POSITION(pj, true)		
+		ENTITY.FREEZE_ENTITY_POSITION(pj, true)
+	else
+		ENTITY.ENABLE_ENTITY_BULLET_COLLISION(pj) -- necessary?
 	end
-
-	ENTITY.ENABLE_ENTITY_BULLET_COLLISION(pj) -- necessary?
 	ENTITY.SET_ALLOW_MIGRATE_TO_SPECTATOR(pj, 1) -- necessary?
 	
 	ENTITY.SET_ENTITY_VISIBLE(entity, false, false)
@@ -93,17 +95,36 @@ function spawn_obj(sc, entity, hash, playerPed)
 	ENTITY.ATTACH_ENTITY_TO_ENTITY(pj, entity, 0, offset.x, offset.y, offset.z, rot.x, rot.y, rot.z, false, false, false, false, 2, true)
 	ENTITY.PROCESS_ENTITY_ATTACHMENTS(entity) -- necessary?
 
+	ENTITY.SET_ENTITY_LOD_DIST(pj, ENTITY.GET_ENTITY_LOD_DIST(entity))	
+
 	local unk = NETWORK.OBJ_TO_NET(pj) -- necessary?
 	return pj
 end
 
-function explode(pj, owner)	
+function handle_explode(sc, master, pj, owner)	
 	local v_coords = ENTITY.GET_ENTITY_COORDS(pj)
+	local trigger_by_master = FIRE.IS_EXPLOSION_IN_SPHERE(2, v_coords.x, v_coords.y, v_coords.z, 3.0)
+	local trigger = false
+	if ENTITY.GET_ENTITY_HEALTH(pj) <= explode_thres then
+		log.info("BOOOOOM by health: "..tostring(pj).." h="..tostring(ENTITY.GET_ENTITY_HEALTH(pj)))
+		trigger = true
+	elseif trigger_by_master then
+		log.info("BOOOOOM by nearby: "..tostring(pj))
+		trigger = true
+	end
+	if not trigger then
+		return
+	end
+	sc:sleep(230) -- when lower than 200 the game ignores the explosion
+	local radius = 0.5
+	MISC.CLEAR_AREA_OF_PROJECTILES(v_coords.x, v_coords.y, v_coords.z, radius, true)
+
 	if owner ~= nil and ENTITY.IS_ENTITY_A_PED(owner) then
 		FIRE.ADD_OWNED_EXPLOSION(owner, v_coords.x, v_coords.y, v_coords.z, c4_exp_choice, 1.0, true, false, false)
 	else
 		FIRE.ADD_EXPLOSION(v_coords.x, v_coords.y, v_coords.z, c4_exp_choice, 1.0, true, false, false)
 	end
+	ENTITY.DELETE_ENTITY(pj)
 end
 
 local pj_record = {}
@@ -117,11 +138,8 @@ script.register_looped("bombloop", function (sc)
 	local pl_coords = ENTITY.GET_ENTITY_COORDS(playerPed)
 	
 	for k, v in pairs(pj_record) do		
-		local v_coords = ENTITY.GET_ENTITY_COORDS(v)
-		if FIRE.IS_EXPLOSION_IN_SPHERE(2, v_coords.x, v_coords.y, v_coords.z, 1.0) or ENTITY.GET_ENTITY_HEALTH(v) <= explode_thres then
-			sc:sleep(225) -- when lower than 200 the game ignores the explosion
-			explode(v, playerPed)
-			ENTITY.DELETE_ENTITY(v)
+		if c4_exp_choice >= 0 and v ~= nil and ENTITY.DOES_ENTITY_EXIST(v) then
+			handle_explode(sc, k, v, nil)
 		end
 		if not ENTITY.DOES_ENTITY_EXIST(k) then
 			if v ~= nil and ENTITY.DOES_ENTITY_EXIST(v) then				
@@ -131,7 +149,7 @@ script.register_looped("bombloop", function (sc)
 		end
 	end
 
-	local pj_obj = OBJECT.GET_CLOSEST_OBJECT_OF_TYPE(pl_coords.x, pl_coords.y, pl_coords.z, 50, before, false, false, false)
+	local pj_obj = OBJECT.GET_CLOSEST_OBJECT_OF_TYPE(pl_coords.x, pl_coords.y, pl_coords.z, 100, before, false, false, false)
 	local has_pj = pj_obj ~= nil and ENTITY.DOES_ENTITY_EXIST(pj_obj)
 
 	if has_pj and pj_record[pj_obj] == nil then
