eb06547e655e4e5aef1927190841dcdcf3510530
George0828Zhang <cc.chang0828@gmail.com>
2023-08-31 16:31:31 +0800

    aaded un-nerfed lazer&hydra cannons. added auto counter measure.

---
+++ AutoCounter.lua
@@ -0,0 +1,125 @@
+local projectile_names = {
+    -- unks:
+    -- xs_prop_arena_airmissile_01a
+    -- gr_prop_gr_missle_long
+    -- gr_prop_gr_missle_short
+    -- "w_arena_airmissile_01a",
+    -- "w_smug_airmissile_01",
+    -- "w_smug_airmissile_01b",
+    -- "w_smug_airmissile_02b",
+    -- "h4_prop_h4_airmissile_01a" -- kosatka maybe
+
+    -- strikeforce
+    "w_battle_airmissile_01",
+    -- nearly all planes/helicopters, tampa, mule c
+    "w_lr_rpg_rocket",
+    -- homing launcher
+    "w_lr_homing_rocket",
+    -- akula, hunter, annihilator s, 
+    "w_smug_airmissile_02",
+    -- oppressor 1 and 2, stromberg, toreador, deluxo, vigilante, thruster, scramjet
+    "w_ex_vehiclemissile_3",
+    -- -- apc & pounder c
+    -- "w_ex_vehiclemissile_2",
+    -- -- aa trailer
+    -- "w_ex_vehiclemissile_1",
+    -- -- chernobog
+    -- "w_ex_vehiclemissile_4",
+}
+local projectiles = {}
+for _, name in pairs(projectile_names) do
+    projectiles[name] = joaat(name)
+end
+local flare_hash = joaat("WEAPON_FLAREGUN")
+local flare_orig = vec3:new(0,-10,0)
+local flare_dests = {
+    vec3:new(0,0,5),
+    vec3:new(5,0,-5),
+    vec3:new(-5,0,-5),
+
+    vec3:new(0,-20,-5),
+    vec3:new(5,-20,5),
+    vec3:new(-5,-20,5),
+}
+local counter_range = 100
+
+function is_approaching(playerPed, pl_coords, proj)
+    local pl_vel = ENTITY.GET_ENTITY_VELOCITY(playerPed)
+    local proj_coords = ENTITY.GET_ENTITY_COORDS(proj)
+    local proj_vel = ENTITY.GET_ENTITY_VELOCITY(proj)
+
+    local v_x = proj_vel.x - pl_vel.x
+    local v_y = proj_vel.y - pl_vel.y
+    local v_z = proj_vel.z - pl_vel.z
+
+    local d_x = pl_coords.x - proj_coords.x
+    local d_y = pl_coords.y - proj_coords.y
+    local d_z = pl_coords.z - proj_coords.z
+    local speed = (v_x * d_x + v_y * d_y + v_z * d_z) / SYSTEM.VMAG(d_x, d_y, d_z)
+
+    return speed > 2 --or speed < -2
+end
+function deploy_once(sc, playerPed)
+    local offset_orig = ENTITY.GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(
+        playerPed, flare_orig.x, flare_orig.y, flare_orig.z)
+    WEAPON.REQUEST_WEAPON_ASSET(flare_hash, 31, 0)
+    while not WEAPON.HAS_WEAPON_ASSET_LOADED(flare_hash) do sc:yield() end
+    for _, flare_dest in pairs(flare_dests) do
+        local offset_dest = ENTITY.GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(
+            playerPed, flare_dest.x, flare_dest.y, flare_dest.z)
+        MISC.SHOOT_SINGLE_BULLET_BETWEEN_COORDS_IGNORE_ENTITY(
+            offset_orig.x,
+            offset_orig.y,
+            offset_orig.z,
+            offset_dest.x,
+            offset_dest.y,
+            offset_dest.z,
+            25, false, flare_hash, playerPed, true, false, 0.1, playerPed, 0) 
+    end
+end
+
+script.register_looped("counterloop", function (sc)
+    sc:yield() -- necessary numbers to update
+     
+    local playerPed = PLAYER.PLAYER_PED_ID()
+    local pl_coords = ENTITY.GET_ENTITY_COORDS(playerPed)
+    
+    for name, v in pairs(projectiles) do
+        local proj = OBJECT.GET_CLOSEST_OBJECT_OF_TYPE(pl_coords.x, pl_coords.y, pl_coords.z, counter_range, v, false, false, false)
+        local has_pj = proj ~= nil and ENTITY.DOES_ENTITY_EXIST(proj)
+
+        local air = ENTITY.IS_ENTITY_IN_AIR(proj)
+        local a_o = ENTITY.IS_ENTITY_ATTACHED_TO_ANY_OBJECT(proj)
+        -- local a_p = ENTITY.IS_ENTITY_ATTACHED_TO_ANY_PED(proj)
+        -- local a_v = ENTITY.IS_ENTITY_ATTACHED_TO_ANY_VEHICLE(proj)
+        if has_pj and air and not a_o then
+            -- log.info("Detected "..name.."("..tostring(proj).." "..tostring(air).." "..tostring(a_o).." "..tostring(a_p).." "..tostring(a_v)..") at "..tostring(proj_coords))
+            if is_approaching(playerPed, pl_coords, proj) then
+                deploy_once(sc, playerPed)
+            end
+        end
+    end
+    -- check each seconds
+    sc:sleep(1000)
+end)
+
+
+-- local myTab = gui.get_tab("Debug")
+-- myTab:add_button("shoot self", function()
+--     local playerPed = PLAYER.PLAYER_PED_ID()
+--     local weap_hash = joaat("weapon_hominglauncher")
+--     local offset_orig = ENTITY.GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(
+--         playerPed, 0, -300, -5)
+--     local offset_dest = ENTITY.GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(
+--         playerPed, 0, 0, 0)
+--     WEAPON.REQUEST_WEAPON_ASSET(weap_hash, 31, 0)
+--     while not WEAPON.HAS_WEAPON_ASSET_LOADED(weap_hash) do script:yield() end
+--     MISC.SHOOT_SINGLE_BULLET_BETWEEN_COORDS_IGNORE_ENTITY_NEW(
+--             offset_orig.x,
+--             offset_orig.y,
+--             offset_orig.z,
+--             offset_dest.x,
+--             offset_dest.y,
+--             offset_dest.z,
+--     100, false, weap_hash, playerPed, true, false, 300.0, nil, false, false, playerPed, false, 0, 0, 0) 
+-- end)
---
+++ JetCannon.lua
@@ -0,0 +1,30 @@
+-- CREDITS goes to 
+-- DMKiller https://www.unknowncheats.me/forum/grand-theft-auto-v/591199-gtav-lazer-hydra-cannon-nerf-bypass.html
+-- nloginov https://www.unknowncheats.me/forum/3831929-post14416.html
+
+-- local tabName = "Jet"
+-- local myTab = gui.get_tab(tabName)
+local target = {0, -1, 0.03999999911}
+local nerfed = {85, 0.125, 0.125}
+
+function patch(a, b, c)
+    local exp_typ_base = memory.scan_pattern("81 7B 10 29 2A 82 E2 ? ? 38 05 ? ? ? ? B8")
+    exp_typ_ptr = exp_typ_base:add(0x10)
+
+    local alt_time_base = memory.scan_pattern("81 7B 10 29 2A 82 E2 ? ? 38 05 ? ? ? ? ? ? F3 0F 10 05 ? ? ? ? ? ? F3 0F 10 83 50")
+    local alt_time_offset = alt_time_base:add(0x15):get_dword()
+    alt_time_ptr = alt_time_base:add(alt_time_offset + 0x19)
+
+    local time_bt_shot_base = memory.scan_pattern("81 7B 10 29 2A 82 E2 ? ? 38 05 ? ? ? ? ? ? F3 0F 10 05 ? ? ? ? ? ? F3 0F 10 83 3C")
+    local time_bt_shot_offset = time_bt_shot_base:add(0x15):get_dword()
+    time_bt_shot_ptr = time_bt_shot_base:add(time_bt_shot_offset + 0x19)
+
+    exp_typ_ptr:set_dword(a)
+    alt_time_ptr:set_float(b)
+    time_bt_shot_ptr:set_float(c)
+end
+
+
+event.register_handler(menu_event.PlayerMgrInit, function ()
+    patch(0, -1, 0.03999999911)
+end)
