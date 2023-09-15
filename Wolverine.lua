09c6bf41a467c0bd986ad19bb0300da667f6e9d3
George0828Zhang <cc.chang0828@gmail.com>
2023-09-15 15:41:57 +0800

    Fixed instant death from collision.

--- Wolverine.lua
+++ Wolverine.lua
@@ -1,10 +1,16 @@
 settings = {
-    heal_enable = true,
-    heal_thres = 285, --128 (very low) 312 (full) set to ~85% to avoid sus
-    heal_rate = {30, 20},
-    armour_enable = true,
-    armour_thres = 35,
-    armour_rate = {8, 10},
+    health={
+        enable=true,
+        low=128, --128 (very low) 312 (full) set to ~85% to avoid sus
+        high=285,
+        rate={30, 20}
+    },
+    armour={
+        enable=true,
+        low=10,
+        high=75,
+        rate={8, 10}
+    },
     headshot_pf = true
 }
 
@@ -17,18 +23,18 @@ Tab:add_imgui(function()
     if ImGui.CollapsingHeader("Regenerative") then
         settings.headshot_pf, _Toggled3 = ImGui.Checkbox("Heatshot Proof", settings.headshot_pf)
         ImGui.SameLine()
-        settings.heal_enable, _Toggled1 = ImGui.Checkbox("Health", settings.heal_enable)
+        settings.health.enable, _Toggled1 = ImGui.Checkbox("Health", settings.health.enable)
         ImGui.SameLine()
-        settings.armour_enable, _Toggled2 = ImGui.Checkbox("Armour", settings.armour_enable)
+        settings.armour.enable, _Toggled2 = ImGui.Checkbox("Armour", settings.armour.enable)
 
-        if settings.heal_enable then
-            settings.heal_rate, _used1 = ImGui.SliderInt2("Regen Rate", settings.heal_rate, 1, 50)
+        if settings.health.enable then
+            settings.health.rate, _used1 = ImGui.SliderInt2("Regen Rate", settings.health.rate, 1, 50)
             if ImGui.IsItemHovered() then
                 ImGui.SetTooltip("Regenerate number1 amount of health for every number2 frames.")
             end
         end
-        if settings.armour_enable then
-            settings.armour_rate, _used2 = ImGui.SliderInt2("Repair Rate", settings.armour_rate, 1, 50)
+        if settings.armour.enable then
+            settings.armour.rate, _used2 = ImGui.SliderInt2("Repair Rate", settings.armour.rate, 1, 50)
             if ImGui.IsItemHovered() then
                 ImGui.SetTooltip("Regain number1 amount of armour for every number2 frames.")
             end
@@ -36,22 +42,33 @@ Tab:add_imgui(function()
     end
 end)
 
+function heal_by_type(playerPed, setting, frame, get_func, set_func)
+    if setting.enable then
+        local curr = get_func(playerPed)
+        local target = curr
+        if curr < setting.low then
+            target = setting.low
+        elseif frame % setting.rate[2] == 0 and curr < setting.high then
+            target = curr + setting.rate[1]
+        end
+        set_func(playerPed, target)
+    end
+end
+
 script.register_looped("wolverineloops", function (script)
     script:yield() -- necessary for the rates to update
     local playerPed = PLAYER.PLAYER_PED_ID()
     if playerPed ~= nil then
-        if settings.heal_enable and _frame % settings.heal_rate[2] == 0 then
-            local x = ENTITY.GET_ENTITY_HEALTH(playerPed)
-            if x <= settings.heal_thres then
-                ENTITY.SET_ENTITY_HEALTH(playerPed, x + settings.heal_rate[1])
-            end
-        end
-        if settings.armour_enable and _frame % settings.armour_rate[2] == 0 then
-            local x = PED.GET_PED_ARMOUR(playerPed)
-            if x <= settings.armour_thres then
-                PED.SET_PED_ARMOUR(playerPed, x + settings.armour_rate[1])
-            end
-        end
+        heal_by_type(
+            playerPed, settings.health, _frame,
+            ENTITY.GET_ENTITY_HEALTH,
+            ENTITY.SET_ENTITY_HEALTH
+        )
+        heal_by_type(
+            playerPed, settings.armour, _frame,
+            PED.GET_PED_ARMOUR,
+            PED.SET_PED_ARMOUR
+        )
         if settings.headshot_pf then
             PED.SET_PED_SUFFERS_CRITICAL_HITS(playerPed, 0)
         end
