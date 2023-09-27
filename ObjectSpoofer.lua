b1410e88347df6c2da2d152d5b171785689cf65a
George0828Zhang <cc.chang0828@gmail.com>
2023-09-27 16:37:53 +0800

    code cleanup

--- ObjectSpoofer.lua
+++ ObjectSpoofer.lua
@@ -4,9 +4,9 @@ presets_text = {
     -- Syntax:
     -- original_model={"new_model", offset(x,y,z), rotation(x,y,z)},
     --
-    w_ex_apmine={"h4_prop_h4_ld_bomb_02a", vec3:new(0, 0, 0), vec3:new(-90, 0, 0)}, -- satchel, earth timer off
+    w_ex_apmine={"h4_prop_h4_ld_bomb_02a", vec3:new(0, 0, 0), vec3:new(-90, 0, 0)},
     w_ex_pe={"prop_cash_pile_01", vec3:new(0, 0, 0), vec3:new(90, 0, 90)},
-    w_ex_pipebomb={"hei_prop_heist_thermite_flash", vec3:new(0, 0, 0), vec3:new(90, 0, 0)}, -- thermite timer on
+    w_ex_pipebomb={"hei_prop_heist_thermite_flash", vec3:new(0, 0, 0), vec3:new(90, 0, 0)},
     
     w_me_gclub={"prop_tool_sledgeham", vec3:new(0, 0, 0), vec3:new(-3, 0, 0)},
     w_me_dagger={"prop_cs_katana_01", vec3:new(0, 0, 0), vec3:new(0, 0, 0)},
@@ -25,20 +25,21 @@ presets_text = {
     w_at_scope_macro={"w_at_scope_nv", vec3:new(-0.05, 0, 0), vec3:new(0, 0, 0)},
 }
 
-function tprint(tbl, indent)
-    if not indent then indent = 0 end
-    for k, v in pairs(tbl) do
-        formatting = string.rep("  ", indent) .. tostring(k) .. ": "
-        if type(v) == "table" then
-            log.info(formatting)
-            tprint(v, indent+1)
-        else
-            log.info(formatting .. tostring(v))
+function spawn_obj(sc, entity, repl_pack)
+    local is_attach = false
+    if ENTITY.IS_ENTITY_ATTACHED(entity) then
+        is_attach = true
+        local at_entity = entity
+        while ENTITY.DOES_ENTITY_EXIST(at_entity) and not ENTITY.IS_ENTITY_A_PED(at_entity) do
+            at_entity = ENTITY.GET_ENTITY_ATTACHED_TO(at_entity)
+        end
+        if ENTITY.DOES_ENTITY_EXIST(at_entity) and at_entity ~= playerPed then
+            return nil
         end
     end
-end
-
-function spawn_obj(sc, entity, hash, offset, rot)
+    local hash = repl_pack[1]
+    local offset = repl_pack[2]
+    local rot = repl_pack[3]
     STREAMING.REQUEST_MODEL(hash)
     while not STREAMING.HAS_MODEL_LOADED(hash) do sc:yield() end
     local pj_coords = ENTITY.GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(entity, 0, 0, 100)
@@ -56,7 +57,7 @@ function spawn_obj(sc, entity, hash, offset, rot)
     STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(hash)
 
     ENTITY.SET_ENTITY_COLLISION(pj, false, false)
-    if ENTITY.IS_ENTITY_ATTACHED(entity) then
+    if is_attach then
         ENTITY.FREEZE_ENTITY_POSITION(pj, true)
     end
     ENTITY.SET_ALLOW_MIGRATE_TO_SPECTATOR(pj, 1) -- necessary?
@@ -65,73 +66,15 @@ function spawn_obj(sc, entity, hash, offset, rot)
     ENTITY.SET_ENTITY_ALPHA(entity, 0, false)
 
     ENTITY.ATTACH_ENTITY_TO_ENTITY(pj, entity, 0, offset.x, offset.y, offset.z, rot.x, rot.y, rot.z, false, false, false, false, 2, true)
-    ENTITY.PROCESS_ENTITY_ATTACHMENTS(entity) -- necessary?
 
     ENTITY.SET_ENTITY_LOD_DIST(pj, ENTITY.GET_ENTITY_LOD_DIST(entity))
 
     return pj
 end
 
-function get_world_addr()
-    local world_base = memory.scan_pattern("48 8B 05 ? ? ? ? 45 ? ? ? ? 48 8B 48 08 48 85 C9 74 07")
-    if world_base:is_null() then
-        log.warning("World address is null! Either the pattern changed or something else is wrong.")
-    end
-    local world_offset = world_base:add(3):get_dword()
-    local world_addr = world_base:add(world_offset + 7)
-    if world_addr:is_null() then
-        log.warning("World address is null! Either the pattern changed or something else is wrong.")
-        return nil
-    end
-    return world_addr:deref()
-end
-
-function get_cped_addr(world_addr)
-    local addr1 = world_addr:add(0x8):deref()
-    if addr1:is_null() then
-        log.warning("CPed address is null! Either the offset changed or something else is wrong.")
-        return nil
-    end
-    return addr1
-end
-
-function get_pos_from_addr(addr)
-    if addr:is_null() then
-        return vec3:new(0,0,0)
-    end
-    local nav_addr = addr:add(0x30):deref()
-    if nav_addr:is_null() then
-        return vec3:new(0,0,0)
-    end
-    local coords_addr = nav_addr:add(0x50)
-    return vec3:new(
-        coords_addr:get_float(),
-        coords_addr:add(4):get_float(),
-        coords_addr:add(8):get_float()
-    )
-end
-
-function get_player_pos()
-    local cped = get_cped_addr(world_addr)
-    if cped:is_null() then
-        return vec3:new(0,0,0)
-    end
-    return get_pos_from_addr(cped)
-end
-
-function get_obj_pos(obj)
-    local addr = memory.handle_to_ptr(obj)
-    return get_pos_from_addr(addr)
-end
-
-function network_owned(obj)
-    return NETWORK.NETWORK_HAS_CONTROL_OF_ENTITY(obj)
-end
-
 script.run_in_fiber(function(script)
     presets = {}
     obj_record = {}
-    world_addr = get_world_addr()
     -- optimize presets
     for k, v in pairs(presets_text) do
         local k_hash = joaat(k)
@@ -152,6 +95,7 @@ end)
 
 script.register_looped("spoofloop", function (sc)
     sc:yield()
+    playerPed = PLAYER.PLAYER_PED_ID()
     -- GC
     for k, v in pairs(obj_record) do
         if not ENTITY.DOES_ENTITY_EXIST(k) then
@@ -163,16 +107,10 @@ script.register_looped("spoofloop", function (sc)
         end
     end
     -- Handle attachment
-    local ppos = get_player_pos()
-    for model,repl_pack in pairs(presets) do
-        local obj = OBJECT.GET_CLOSEST_OBJECT_OF_TYPE(ppos.x, ppos.y, ppos.z, proximity, model, false, false, false)
-        local has_obj = obj ~= nil and ENTITY.DOES_ENTITY_EXIST(obj)
-        if has_obj and obj_record[obj] == nil and network_owned(obj) then
-            local repl_md = repl_pack[1]
-            local repl_off = repl_pack[2]
-            local repl_rot = repl_pack[3]
-            local repl_obj = spawn_obj(sc, obj, repl_md, repl_off, repl_rot)
-            obj_record[obj] = repl_obj
+    for _, obj in pairs(entities.get_all_objects_as_handles()) do
+        local repl_pack = presets[ENTITY.GET_ENTITY_MODEL(obj)]
+        if repl_pack ~= nil and obj_record[obj] == nil then
+            obj_record[obj] = spawn_obj(sc, obj, repl_pack)
         end
     end
 end)
